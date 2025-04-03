81c0d9e0f392c694f8eb9ee1131658af63788013
riley-ac <rchartrand@student.neumont.edu>
2025-04-03 16:10:00 -0600

    added a Employee class

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -3,23 +3,63 @@
 
 #include "Student.h"
 #include "iostream"
+#include "xstring"
 
 
+class Person
+{
+public:
+    std::string name;
+    int age;
+    float weight;
 
+    void f() {}
+};
 
 
 
 
 int main()
 {
+
+    using namespace std;
     Student student;
     student.SetName("bob");
     f();
     
-    std::cout << "Hello World\n";
+   
     
-    return 0;
-    
+    Person person; 
 
+    person.age = 19;
+
+    unsigned int i = 82;
+  //  printf("%d\n", i);
+    std::cout << i << std::endl;
+    std::cout << sizeof(char) << std::endl;
+    std::cout << sizeof(Person) << std::endl;
+
+    i++;
+    i + i + 5;
+
+
+    char c = 'A';
+    cout << &i << endl;
+
+
+
+
+    f();
+
+
+
+
+
+
+
+
+    std::cout << "Hello World\n";
+
+    return 0;
 }
 
